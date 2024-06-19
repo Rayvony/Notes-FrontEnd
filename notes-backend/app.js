6f908e1f0cbd6b40f2df9649b8ebacccbb179656
Mariano Vargas <marianoluisvargas@gmail.com>
2024-06-19 12:35:41 -0300

    fix(cors)

--- notes-backend/app.js
+++ notes-backend/app.js
@@ -7,16 +7,20 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  const allowedOrigins = ["http://localhost:3000", "https://notes-front-ridlyzcv1-rayvonys-projects.vercel.app/"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  cors({
-    origin: process.env.FRONTEND_ORIGIN,
-    methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-  })
-);
 
 sequelize
   .sync()
